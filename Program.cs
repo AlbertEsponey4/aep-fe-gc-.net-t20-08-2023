b0d4d8b79069c18ce244dddc7c234135c438facd
AlbertEsponey4 <126797937+AlbertEsponey4@users.noreply.github.com>
2023-08-10 15:25:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Tarea19
+{
+    class Program
+    {
+        static void Ejercicio1()
+        {
+            int A = 3;
+            int B = 4;
+            int C = 5;
+
+            int suma = A + B + C;
+        }
+
+        static void Ejercicio2()
+        {
+
+        }
+
+        static void Ejercicio3()
+        {
+
+        }
+
+        static void Ejercicio4()
+        {
+
+        }
+
+        static void Ejercicio5()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
